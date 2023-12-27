7611fd50d7c89b785dd31b3bfeb736933c7933cf
batikinha <abbatikinha@gmail.com>
2023-12-27 14:20:46 -0300

    change4

--- kitty-pointer.js
+++ kitty-pointer.js
@@ -3,18 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const kittyPointer = document.getElementById("kitty-pointer");
 
     body.addEventListener("mousemove", function (e) {
-        // Posiciona o ponteiro de gatinho nas coordenadas do cursor
         kittyPointer.style.left = e.pageX + "px";
         kittyPointer.style.top = e.pageY + "px";
     });
 
     body.addEventListener("mouseenter", function () {
-        // Adiciona efeito de arco-íris quando o mouse entra na tela
         kittyPointer.classList.add("rainbow-effect");
     });
 
     body.addEventListener("mouseleave", function () {
-        // Remove o efeito de arco-íris quando o mouse sai da tela
         kittyPointer.classList.remove("rainbow-effect");
     });
 });
